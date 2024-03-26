f78bd8193658e2aaa6d9414f65a3ed4a3607fd6e
Hedwig16 <srivishnu91500@gmail.com>
2024-03-26 19:02:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Dialogflow Messenger integration */}
+      <df-messenger
+        project-id="emschartchatbot"
+        agent-id="05924197-6bbd-4f08-881c-16bacb3e1f89"
+        language-code="en"
+        max-query-length="-1"
+        allow-feedback="all">
+        <df-messenger-chat-bubble chat-title="EMS-Chart-Agent" class="custom-chat-bubble">
+          {/* No need for an <img> tag here */}
+        </df-messenger-chat-bubble>
+      </df-messenger>
     </div>
   );
 }
 
 export default App;
+
